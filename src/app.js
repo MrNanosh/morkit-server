73d6f1f5f78c3ba2b823ed5b464fc85af4bf8aaa
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-12-31 09:18:42 -0800

    implemented a skeleton with dummy data from app

--- src/app.js
+++ src/app.js
@@ -13,15 +13,54 @@ const morganOption =
   NODE_ENV === 'production'
     ? 'tiny'
     : 'common';
-// const router = require('./service-routers/router');
+// const message-router = require('./message/message-router');
+// const router = require('./inventory/inventory-router');
+// const router = require('./user/user-router');
+// const router = require('./forsale/forsale-router');
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('helloworld');
 });
+app.get(
+  '/api/forsale',
+  (req, res, next) => {
+    // console.log('forsale');
+    res.json([
+      {
+        id: 1,
+        item_name: 'item1',
+        item_body:
+          'this is an original item1, for sale, low price',
+        item_owner: 7,
+        item_is: 'selling'
+      },
+      {
+        id: 2,
+        item_name: 'item2',
+        item_body:
+          'this is an original item2, for sale, low price',
+        item_owner: 6,
+        item_is: 'selling'
+      }
+    ]);
+  }
+);
+app.get('/api/message', (req, res) => {
+  res.json(['these messages']);
+});
+app.get(
+  '/api/inventory',
+  (req, res) => {
+    res.json(['things you have']);
+  }
+);
+// app.use('/api/message', message-router)
+// app.use('/api/inventory', inventory-router)
+// app.use('/api/user', user-router)
+// app.use('/api/forsale', forsale-router)
 
 app.use(function errorHandler(
   error,
@@ -43,7 +82,5 @@ app.use(function errorHandler(
   }
   res.status(500).json(response);
 });
-
-// app.use('/service-routers', router);
 
 module.exports = app;
